203188bb01f7d497082a006b9ba18294f20e5910
Luiz Henrique <lu1zh3nr1qu34@gmail.com>
2020-12-02 15:27:11 -0300

    Client class refactored, TestClient created, few bugs fixed

---
+++ HashTable/src/main/java/com/client/TestClient.java
@@ -0,0 +1,39 @@
+package com.client;
+
+import com.hashTable.KeyValue;
+
+public class TestClient extends ClientConnection{
+    private static final int TESTS = 1000;
+
+    public static void main(String []args){}
+
+    @Override
+    public void set() {
+
+    }
+
+    @Override
+    public void get() {
+
+    }
+
+    @Override
+    public void del() {
+
+    }
+
+    @Override
+    public void delKeyVersion() {
+
+    }
+
+    @Override
+    public void testAndSet() {
+
+    }
+
+    @Override
+    public void displayResponse(KeyValue.Response response) {
+
+    }
+}
