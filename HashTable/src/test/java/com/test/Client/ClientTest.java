57cedcf5b4ab7ee011b24cde92774ba083a8b91f
Luiz Henrique <lu1zh3nr1qu34@gmail.com>
2020-12-03 15:07:55 -0300

    Client test

--- HashTable/src/test/java/com/test/Client/ClientTest.java
+++ HashTable/src/test/java/com/test/Client/ClientTest.java
@@ -5,58 +5,58 @@ import com.google.protobuf.ByteString;
 import com.hashTable.KeyValue;
 import com.utils.LongHandler;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ClientTest extends ClientConnection {
     private static final int TESTS = 1000;
 
     @BeforeEach
-    void connect(){
+    void init(){
         connectToServer();
     }
 
-    @Override
     @Test
     @RepeatedTest(TESTS)
+    @DisplayName("SET FUNCTION")
+    @Override
     public void set() {
         ByteString key, data;
-        key = data = LongHandler.convertFromLongToByteString(generateRandomLong(1L, 500L));
+        key = data = LongHandler.convertFromLongToByteString(generateRandomLong(1L, 1000L));
         long timestamp = getCurrentTimestamp();
         KeyValue.Set setRequest = createSetRequest(key, data, timestamp);
         KeyValue.Response response = clientKeyValueStub.set(setRequest);
-        assertEquals("SUCCESS", response.getMessage());
+        assertTrue(response.getMessage().equals("SUCCESS"), response.getMessage());
     }
 
     @Override
-    public void get() {
+    public void get() {}
 
-    }
-
+    @Test
+    @RepeatedTest(TESTS)
+    @DisplayName("DEL FUNCTION")
     @Override
     public void del() {
         ByteString key;
-        for (int i = 0; i < TESTS; i++){
-            key = LongHandler.convertFromLongToByteString(generateRandomLong(1L, 500L));
-            KeyValue.Del delRequest = createDelRequest(key);
-            KeyValue.Response response = clientKeyValueStub.del(delRequest);
-            displayResponse(response);
-
-        }
+        key = LongHandler.convertFromLongToByteString(generateRandomLong(1L, 500L));
+        KeyValue.Del delRequest = createDelRequest(key);
+        KeyValue.Response response = clientKeyValueStub.del(delRequest);
+        assertTrue(response.getMessage().equals("SUCCESS"), response.getMessage());
     }
 
+    @Test
+    @RepeatedTest(TESTS)
+    @DisplayName("DELKEYVERSION TEST")
     @Override
     public void delKeyVersion() {
         ByteString key;
-        for (int i = 0; i < TESTS; i++){
-            key = LongHandler.convertFromLongToByteString(generateRandomLong(1L, 500L));
-            long version = generateRandomLong(1L, 10L);
-            KeyValue.Del delRequest = createDelRequest(key, version);
-            KeyValue.Response response = clientKeyValueStub.del(delRequest);
-            displayResponse(response);
-        }
+        key = LongHandler.convertFromLongToByteString(generateRandomLong(1L, 500L));
+        long version = generateRandomLong(1L, 10L);
+        KeyValue.Del delRequest = createDelRequest(key, version);
+        KeyValue.Response response = clientKeyValueStub.del(delRequest);
+        assertTrue(response.getMessage().equals("SUCCESS"), response.getMessage());
     }
 
     @Override
